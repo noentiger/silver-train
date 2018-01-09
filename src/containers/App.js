c97062b170798270e773f6500d6ef45cc16ace0d
Kim Wijk <kim.wijk@gmail.com>
2018-01-09 14:51:32 +0100

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import FirstPage from './FirstPage'
+import SecondPage from './SecondPage'
+import NoMatch from '../components/NoMatch'
+
+export default class App extends Component {
+  render(){
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={FirstPage}/>
+          <Route path="/second" component={SecondPage}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </div>
+    )
+  }
+}
---
+++ src/containers/FirstPage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import * as userActions from '../actions/user'
+import { Link } from 'react-router-dom'
+import './FirstPage.css'
+
+class FirstPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>{`Email: ${this.props.user.email}`}</p>
+        <Link to={'/second'}>Second</Link>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActions, dispatch)
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FirstPage)
