b8c13011cb5e5d0c3756c46e11cce06066f9a41f
Kim Wijk <kim.wijk@gmail.com>
2018-01-10 11:28:11 +0100

    Add fb tags and structured data

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,29 @@
 export default {
-  artistName: 'Kim Wijk',
+  artist: {
+    name: 'Kim Wijk',
+    images: [
+      'http://kim.wijk.com/images/kim-wijk-cover-window.jpg',
+      'http://kim.wijk.com/images/kim-wijk-cover-snow.jpg',
+    ],
+    tracks: [
+      {
+        '@type': 'MusicRecording',
+        'audio': 'https://open.spotify.com/track/5A19jKYDsMckBCltYXkpVK',
+        'duration': 'PT3M47S',
+        'inAlbum': 'Riv mig itu',
+        'name': 'Riv mig itu',
+        'url': 'https://open.spotify.com/track/5A19jKYDsMckBCltYXkpVK',
+      },
+    ],
+  },
   urls: {
     facebook: 'https://www.facebook.com/wijkim/',
     spotify: 'https://open.spotify.com/artist/0MegUhRAG8cKnF8PZ3zsP7',
+  },
+  meta: {
+    title: 'Kim Wijk',
+    image: 'http://kim.wijk.com/images/kim-wijk-profile-picture.jpg',
+    url: 'http://kimwijk.com',
+    description: 'Svensk indie pop/rock. Lyssna på senaste singeln Riv Mig Itu på Spotify.',
   },
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,11 +6,38 @@ import NoMatch from '../components/NoMatch'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Helmet from 'react-helmet';
+import constants from '../constants';
 
 export default class App extends Component {
-  render(){
+
+  render() {
     return (
       <div>
+        <Helmet
+          title={constants.meta.title}
+          meta={[
+              { property: 'og:title', content: constants.meta.title },
+              { property: 'og:image', content: constants.meta.image },
+              { property: 'og:site_name', content: constants.meta.title },
+              { property: 'og:url', content: constants.meta.url },
+              { property: 'og:type', content: 'article' },
+              { property: 'og:description', content: constants.meta.description },
+              { name: 'description', content: constants.meta.description },
+          ]}
+          script={[
+            {
+              type: 'application/ld+json',
+              innerHTML: `{
+                  "@context": "http://schema.org",
+                  "@type": "MusicGroup",
+                  "name": "${constants.artist.name}",
+                  "image": ${JSON.stringify(constants.artist.images)},
+                  "track": ${JSON.stringify(constants.artist.tracks)}
+              }`,
+            },
+          ]}
+        />
         <Grid fluid>
           <Header />
           <Row center="xs">
