8f91bf26a8069db1aa0d1a208018c0a112655f16
Kim Wijk <kim.wijk@gmail.com>
2018-01-11 15:47:45 +0100

    Add Facebook Pixel

---
+++ src/components/HelmetData.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import constants from '../constants';
+
+const HelmetData = () => (
+  <Helmet>
+    <title>{constants.meta.title}</title>
+    <meta property="og:title" content={constants.meta.title} />
+    <meta property="og:image" content={constants.meta.image} />
+    <meta property="og:site_name" content={constants.meta.title} />
+    <meta property="og:url" content={constants.meta.url} />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content={constants.meta.description} />
+    <meta name="description" content={constants.meta.description} />
+    <meta property="fb:app_id" content={constants.meta.facebookAppId} />
+
+    <script type="application/ld+json">
+      {`
+        {
+          "@context": "http://schema.org",
+          "@type": "MusicGroup",
+          "name": "${constants.artist.name}",
+          "image": ${JSON.stringify(constants.artist.images)},
+          "track": ${JSON.stringify(constants.artist.tracks)}
+        }
+      `}
+    </script>
+
+    <script type="text/javascript">
+      {
+        `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${constants.analytics.facebookPixel}');
+          fbq('track', 'PageView');
+        `
+      }
+    </script>
+
+    <noscript>
+      {`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${constants.analytics.facebookPixel}&ev=PageView&noscript=1"/>`}
+    </noscript>
+
+  </Helmet>
+);
+
+export default HelmetData;
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,39 +6,14 @@ import NoMatch from '../components/NoMatch'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Helmet from 'react-helmet';
-import constants from '../constants';
+import HelmetData from '../components/HelmetData';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Helmet
-          title={constants.meta.title}
-          meta={[
-              { property: 'og:title', content: constants.meta.title },
-              { property: 'og:image', content: constants.meta.image },
-              { property: 'og:site_name', content: constants.meta.title },
-              { property: 'og:url', content: constants.meta.url },
-              { property: 'og:type', content: 'article' },
-              { property: 'og:description', content: constants.meta.description },
-              { name: 'description', content: constants.meta.description },
-              { property: 'fb:app_id', content: constants.meta.facebookAppId },
-          ]}
-          script={[
-            {
-              type: 'application/ld+json',
-              innerHTML: `{
-                  "@context": "http://schema.org",
-                  "@type": "MusicGroup",
-                  "name": "${constants.artist.name}",
-                  "image": ${JSON.stringify(constants.artist.images)},
-                  "track": ${JSON.stringify(constants.artist.tracks)}
-              }`,
-            },
-          ]}
-        />
+        <HelmetData />
         <Grid fluid>
           <Header />
           <Row center="xs">
