44c6744189bea0ec8c37aeaf8515acad425aee88
Kim Wijk <kim.wijk@gmail.com>
2018-01-11 16:14:03 +0100

    Revert "Add Facebook Pixel"
    This reverts commit 8f91bf26a8069db1aa0d1a208018c0a112655f16.

--- src/constants.js
+++ src/constants.js
@@ -29,6 +29,5 @@ export default {
   },
   analytics: {
     google: 'UA-112226557-1',
-    facebookPixel: '2062150904005387',
   },
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,14 +6,39 @@ import NoMatch from '../components/NoMatch'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import HelmetData from '../components/HelmetData';
+import Helmet from 'react-helmet';
+import constants from '../constants';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <HelmetData />
+        <Helmet
+          title={constants.meta.title}
+          meta={[
+              { property: 'og:title', content: constants.meta.title },
+              { property: 'og:image', content: constants.meta.image },
+              { property: 'og:site_name', content: constants.meta.title },
+              { property: 'og:url', content: constants.meta.url },
+              { property: 'og:type', content: 'article' },
+              { property: 'og:description', content: constants.meta.description },
+              { name: 'description', content: constants.meta.description },
+              { property: 'fb:app_id', content: constants.meta.facebookAppId },
+          ]}
+          script={[
+            {
+              type: 'application/ld+json',
+              innerHTML: `{
+                  "@context": "http://schema.org",
+                  "@type": "MusicGroup",
+                  "name": "${constants.artist.name}",
+                  "image": ${JSON.stringify(constants.artist.images)},
+                  "track": ${JSON.stringify(constants.artist.tracks)}
+              }`,
+            },
+          ]}
+        />
         <Grid fluid>
           <Header />
           <Row center="xs">
